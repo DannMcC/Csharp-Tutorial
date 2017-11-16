db0d0441fe6d3ab8446b40b082cac0ca4e5e3277
Dan McCune <megadannn@gmail.com>
2017-11-15 22:01:52 -0500

    hello world

---
+++ Tutorial/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string response = Console.ReadLine();
+            Console.WriteLine("Hello " + response);
+            Console.WriteLine("Press enter to close...");
+            Console.ReadLine();
+        }
+    }
+}
