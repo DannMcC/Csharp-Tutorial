fd92fbad0a4f6a6abe5e2b31aea5160ecfeabf2d
Dan McCune <megadannn@gmail.com>
2017-11-16 15:21:12 -0500

    using var to assign default types

--- Scope/Program.cs
+++ Scope/Program.cs
@@ -6,12 +6,12 @@ namespace Scope
     {
         static void Main(string[] args)
         {
-            byte number = 2;
-            int count = 10;
-            float totalPrice = 20.95f;
-            char character = 'A';
-            string firstName = "Dan";
-            bool isWorking = false;
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95f;
+            var character = 'A';
+            var firstName = "Dan";
+			var isWorking = false;
 
             Console.WriteLine(number);
             Console.WriteLine(count);
